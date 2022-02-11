d4f15129add7c90a7448b724a75dd8bf61c6d1d9
elencha3 <elenadorch@gmail.com>
2022-02-11 17:50:55 +0000

    Initial Commit

---
+++ Ej04_Esther_Lenador/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ej04_Esther_Lenador
+//
+//  Created by user195954 on 2/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
