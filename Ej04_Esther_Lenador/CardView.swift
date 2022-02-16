55577d83a363ab5f57a12d77608bc7c70a20d9ce
elencha3 <elenadorch@gmail.com>
2022-02-16 17:21:05 +0000

    scrollview

--- Ej04_Esther_Lenador/CardView.swift
+++ Ej04_Esther_Lenador/CardView.swift
@@ -27,95 +27,98 @@ struct CardView: View {
     
     
     var body: some View {
-        VStack  {
-            VStack {
-                Text("Personaliza tu tarjeta")
-                    .font(Font.custom("Montserrat-Light", size: 30))
-                    .padding(.top,30)
-                ZStack {
-                    VStack {
-                        Text(userInfo.name.prefix(1))
-                            .font(.largeTitle)
-                            .frame(width: 80, height: 80)
-                            .background(bgCircleColor)
-                            .clipShape(Circle())
-                            .padding()
-                        Spacer().frame(height:5)
-                        VStack(alignment: .leading) {
-                            if !flipped {
-                                Text(userInfo.name + " " + userInfo.surname)
-                                Text(userInfo.job)
-                            } else {
-                                Text(userInfo.phone)
-                                Text(userInfo.email)
-                                Text(userInfo.adress)
+        ScrollView {
+            VStack  {
+                VStack {
+                    Text("Personaliza tu tarjeta")
+                        .font(Font.custom("Montserrat-Light", size: 30))
+                        .padding(.top,30)
+                    ZStack {
+                        VStack {
+                            Text(userInfo.name.prefix(1))
+                                .font(.largeTitle)
+                                .frame(width: 80, height: 80)
+                                .background(bgCircleColor)
+                                .clipShape(Circle())
+                                .padding()
+                            Spacer().frame(height:5)
+                            VStack(alignment: .leading) {
+                                if !flipped {
+                                    Text(userInfo.name + " " + userInfo.surname)
+                                    Text(userInfo.job)
+                                } else {
+                                    Text(userInfo.phone)
+                                    Text(userInfo.email)
+                                    Text(userInfo.adress)
+                                }
+                            }
+                        }.frame(width:300, height: 200)
+                        .foregroundColor(fgColor)
+                        .background(bgColor)
+                        .cornerRadius(16)
+                        .shadow(color: Color.gray, radius: 16, x:14, y:14)
+                        .rotation3DEffect(Angle(degrees:self.flipped ? 180 : 0), axis: (x:0,y:1,z:0))
+                        .onTapGesture {
+                            withAnimation(.easeIn(duration: 0.5)){
+                                self.flipped.toggle()
                             }
                         }
-                    }.frame(width:300, height: 200)
-                    .foregroundColor(fgColor)
-                    .background(bgColor)
-                    .cornerRadius(16)
-                    .shadow(color: Color.gray, radius: 16, x:14, y:14)
+                    }.padding(.top, 20)
+                    .font(Font.custom(customFont, size: 16))
                     .rotation3DEffect(Angle(degrees:self.flipped ? 180 : 0), axis: (x:0,y:1,z:0))
                     .onTapGesture {
                         withAnimation(.easeIn(duration: 0.5)){
                             self.flipped.toggle()
                         }
                     }
-                }.padding(.top, 20)
-                .font(Font.custom(customFont, size: 16))
-                .rotation3DEffect(Angle(degrees:self.flipped ? 180 : 0), axis: (x:0,y:1,z:0))
-                .onTapGesture {
-                    withAnimation(.easeIn(duration: 0.5)){
-                        self.flipped.toggle()
-                    }
                 }
-            }
-            Divider().frame(height: 40)
-            
-            
-            VStack {
-                Text("Colores").frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 30, alignment: .leading)
-                    .padding(.horizontal, 20)
-                    .font(Font.custom("Montserrat-Medium", size: 20))
-                HStack {
-                    ForEach(colorArray, id:\.self) { color in
-                        Circle()
-                            .strokeBorder(bgColor == color ? Color.black : Color.clear, lineWidth: 2)
-                            .background(Circle().fill(color))
-                            .frame(width: 50, height: 50)
-                            .onTapGesture {
-                                bgColor = color
-                            }
+                Divider().frame(height: 40)
+                
+                
+                VStack {
+                    Text("Colores").frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 30, alignment: .leading)
+                        .padding(.horizontal, 20)
+                        .font(Font.custom("Montserrat-Medium", size: 20))
+                    HStack {
+                        ForEach(colorArray, id:\.self) { color in
+                            Circle()
+                                .strokeBorder(bgColor == color ? Color.black : Color.clear, lineWidth: 2)
+                                .background(Circle().fill(color))
+                                .frame(width: 50, height: 50)
+                                .onTapGesture {
+                                    bgColor = color
+                                }
+                        }
                     }
+                    VStack {
+                        ColorPicker("Más colores de fondo", selection: $bgColor).frame(width:300, height:30)
+                        ColorPicker("Color de texto", selection: $fgColor).frame(width:300, height:20).padding(.bottom, 5)
+                        ColorPicker("Color de círculo", selection: $bgCircleColor).frame(width:300, height:20)
+                    }.font(Font.custom("Montserrat-Light", size: 15))
+                    .padding(.top,10)
                 }
                 VStack {
-                    ColorPicker("Más colores de fondo", selection: $bgColor).frame(width:300, height:30)
-                    ColorPicker("Color de texto", selection: $fgColor).frame(width:300, height:20).padding(.bottom, 5)
-                    ColorPicker("Color de círculo", selection: $bgCircleColor).frame(width:300, height:20)
-                }.font(Font.custom("Montserrat-Light", size: 15))
-                .padding(.top,10)
-            }
-            VStack {
-                Text("Fuentes").frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 30, alignment: .leading)
+                    Text("Fuentes").frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 30, alignment: .leading)
+                        .padding(.horizontal,20)
+                        .font(Font.custom("Montserrat-Medium", size: 20))
+                    HStack {
+                        Text(customFont)
+                        Spacer()
+                        Picker("Cambiar", selection:$customFont) {
+                            ForEach(fontArray, id:\.self) {
+                                Text($0)
+                            }
+                        }.pickerStyle(MenuPickerStyle())
+                    }
+                    .font(Font.custom("Montserrat-Light", size: 15))
                     .padding(.horizontal,20)
-                    .font(Font.custom("Montserrat-Medium", size: 20))
-                HStack {
-                    Text(customFont)
-                    Spacer()
-                    Picker("Cambiar", selection:$customFont) {
-                        ForEach(fontArray, id:\.self) {
-                            Text($0)
-                        }
-                    }.pickerStyle(MenuPickerStyle())
-                }
-                .font(Font.custom("Montserrat-Light", size: 15))
-                .padding(.horizontal,20)
-                
-            }.padding(.top, 30)
-            Spacer()
-        }.font(Font.custom("Montserrat-Light", size: 20))
-    }
+                    
+                }.padding(.top, 30)
+                Spacer()
+            }.font(Font.custom("Montserrat-Light", size: 20))
+        }
+        }
+        
 }
 
 
