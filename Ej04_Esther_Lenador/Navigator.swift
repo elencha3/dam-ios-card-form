b16a413aa06179d7bee16e450e1d61e279ee9e69
elencha3 <elenadorch@gmail.com>
2022-02-18 18:10:44 +0000

    Splash screen

--- Ej04_Esther_Lenador/Navigator.swift
+++ Ej04_Esther_Lenador/Navigator.swift
@@ -3,22 +3,42 @@
 import SwiftUI
 
 struct Navigator: View {
+    
+    @State var showMainView:Bool = false
+    
     var body: some View {
-        
-        
-        TabView{
-            FormView()
-                .tabItem {
-                    Image(systemName: "person.crop.square")
-                    Text("Form")
+        VStack {
+            if self.showMainView {
+                TabView{
+                    FormView()
+                        .tabItem {
+                            Image(systemName: "person.crop.square")
+                            Text("Form")
+                        }
+                    CardView()
+                        .tabItem {
+                            Image(systemName: "rectangle.on.rectangle.angled")
+                            Text("Card")
+                        }
                 }
-            CardView()
-                .tabItem {
-                    Image(systemName: "rectangle.on.rectangle.angled")
-                    Text("Card")
+            } else {
+                Text("Create Your Card")
+                    .foregroundColor(Color.yellow)
+                    .font(Font.custom(("Montserrat-Light"), size: 100))
+                    .bold()
+            }
+        }.onAppear {
+            // after three seconds, system will change the value of the
+            // showMainView variable
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                withAnimation {
+                    self.showMainView = true
                 }
+            }
         }
+        
     }
+    
 }
 
 
